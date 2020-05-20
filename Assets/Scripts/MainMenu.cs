2b4b50293470936413cb891f5d56e06a46ae2e8c
sranciato <sranciato1@gmail.com>
2020-05-20 17:08:22 -0400

    [ADD] colorblind

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
 
+	public Material trapMat;
+	public Material goalMat;
+	public Toggle colorblindMode;
 	public void PlayMaze()
 	{
+		if (colorblindMode.isOn)
+		{
+			trapMat.color = new Color32(255, 112, 0, 1);
+			goalMat.color = Color.blue;
+		}
+		else
+		{
+			trapMat.color = Color.red;
+			goalMat.color = Color.green;
+		}
 		SceneManager.LoadScene(1);
 	}
 
